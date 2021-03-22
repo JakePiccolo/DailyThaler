54721514e5d401a7466c2bc74b24dc746449abf0
jake piccolo <icepiccolo@yahoo.com>
2021-03-22 09:12:21 -0400

    Changed to embedded and added urls

--- DailyThaler.py
+++ DailyThaler.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import discord # Discord API
 from discord.ext import commands
 
-TOKEN = 'insert token'
+TOKEN = 'insert token here'
 GUILD = 'BoizBoizBoiz'
 
 # Create dice bot and register commands
@@ -20,9 +20,9 @@ def getthaler(soup, topn):
         try:
             what = data[0].a.text
             when = data[0].a.find_next_sibling().text
-            #url = data[0].a
-            #url = url['href']
-            wlist.append(what + ', ' + when)
+            url = data[0].a
+            url = url['href']
+            wlist.append(what + ', ' + when + ', ' + 'https://runescape.wiki' + url)
             lim = lim + 1
             if lim > topn: break 
         except IndexError:pass
@@ -41,16 +41,23 @@ async def quests(ctx):
     res = requests.get(URL).text
     soup = BeautifulSoup(res,'lxml')
     try:
-        await ctx.send('{0}, Here are the next ten Thaler Minigames:'.format(author.mention))
+        embed = discord.Embed(title = "Thaler Minigames", 
+        description = "These are the next ten Minigames:", 
+        color = discord.Color.red(),
+        icon_url = 'https://static.wikia.nocookie.net/runescape2/images/b/b1/Minigame_Spotlight_update_post_header.jpg/revision/latest/scale-to-width-down/629?cb=20150518110749'
+        )
+        #await ctx.send('{0}, Here are the next ten Thaler Minigames:'.format(author.mention))
         questlist = []
         for quest in getthaler(soup, topn = 10):
             questlist.append(quest)
-        await ctx.send('\n'.join(questlist))
-        url = getupdate(soup)
-        await ctx.send('Updates in '+ url)
+        #await ctx.send('\n'.join(questlist))
+        embed.add_field(name = "Quest List", value = '\n'.join(questlist), inline = False )
+
+        upd = getupdate(soup)
+        embed.add_field(name = "Updates in ", value = upd, inline = True)
+        #await ctx.send('Updates in '+ upd)
+        await ctx.send(embed = embed)
     except Exception as err: 
         await ctx.send('Sorry that did not work {0}. Here is the issue: {1}'.format(author.mention, err))
 
 bot.run(TOKEN)
-
-doit()
