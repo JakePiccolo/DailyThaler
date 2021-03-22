9cdb05cf996b62328c6e4ca997de278b956b55d9
jake piccolo <icepiccolo@yahoo.com>
2021-03-21 22:07:06 -0400

    Fixed venv

--- DailyThaler.py
+++ DailyThaler.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import discord # Discord API
 from discord.ext import commands
 
-TOKEN = 'insert token here'
+TOKEN = 'insert token'
 GUILD = 'BoizBoizBoiz'
 
 # Create dice bot and register commands
